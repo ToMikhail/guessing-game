c3a8b0c024f16862cdd3aed2ca6ef3b56559a8d7
To Mikha <toustsikmikhail@gmail.com>
2019-10-12 20:23:24 +0300

    feat:add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,57 @@
 class GuessingGame {
-    constructor() {}
 
-    setRange(min, max) {
+        constructor() {
+            this.min = 0;
+            this.max = 0;
+            this.number=0;
+        }
 
-    }
+        setRange(min, max) {
+            this.min = min;
+            this.max = max;
+        }
 
-    guess() {
+        guess() {
+            this.number = Math.round((this.max - this.min) / 2) + this.min
+            return this.number
+        }
 
-    }
+        lower() {
+            this.max = this.number 
+        }
 
-    lower() {
+        greater() {
+            this.min = this.number
+        }
 
-    }
-
-    greater() {
-
-    }
 }
 
 module.exports = GuessingGame;
+
+        // const number = 409;
+        // const game = new GuessingGame();
+        // game.setRange(0, 4048)
+
+        // let result = game.guess();
+        // game.lower();
+        // result = game.guess();
+        // game.lower();
+        // result = game.guess();
+        // game.lower();
+        // result = game.guess();
+        // game.greater();
+        // result = game.guess();
+        // game.greater();
+        // result = game.guess();
+        // game.lower();
+        // result = game.guess();
+        // game.lower();
+        // result = game.guess();
+        // game.greater();
+        // result = game.guess();
+        // game.greater();
+        // result = game.guess();
+        // game.greater();
+        // result = game.guess();
+        // game.lower();
+        // result = game.guess()
